140a0f3217ef90f429739a23fb7cdc7287020000
Anja Fullgrabe <anjaf@ebi.ac.uk>
2021-03-12 12:34:19 +0000

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="atlas-metadata-validator",
-    version="1.4.1",
+    version="1.5.0",
     author="Anja Füllgrabe",
     author_email="anjaf@ebi.ac.uk",
     description="A MAGE-TAB validator for Expression Atlas and Single Cell Expression Atlas",
