7eeb2ad77c65c5e3b3abb0ab7b5d03a3cc69cac8
Anja Fullgrabe <anjaf@ebi.ac.uk>
2021-10-11 17:19:13 +0100

    Serialise FTP file checks under one connection

--- atlas_metadata_validator/fetch.py
+++ atlas_metadata_validator/fetch.py
@@ -9,6 +9,9 @@ import urllib
 import socket
 import git
 import ftplib
+from collections import defaultdict
+import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor as PoolExecutor
 
 # To store organisms that we have already looked-up in the taxonomy (this is slow...)
 organism_lookup = {}
@@ -42,58 +45,92 @@ def get_taxon(organism, logger=logging.getLogger()):
         return organism_lookup.get(organism)
 
 
-def is_valid_ftp(url, logger=None):
-    """Check if a given FTP file exists without downloading the page/file
+def check_ftp_uris(ftp_uris, invalid_uris, logger=None):
+    """Check if a dict of given FTP files exist without downloading the page/file
+    List of FTP addresses are listed by domain. We establish one connection to the FTP server
+    and then check the paths if they exist. If not report the uri as invalid.
+
+    :param ftp_uris
+    input dict structure:
+    dictionary with URI field (like "FASTQ_URI"), then domain (like "ftp.sra.ebi.ac.uk")
+    values are lists of dicts with
+    {"uri": "ftp://ftp...", "path":"/vol1/fastq/..."}
+
+    :param invalid_uris
+    dictionary to hold the invalid uris, lists by URI field
     """
 
-    parsed = urllib.parse.urlparse(url)
-    domain = parsed.netloc
-    path = parsed.path
-    scheme = parsed.scheme
-    if scheme != 'ftp':
-        raise Exception("{} is not an FTP link".format(url))
-    f = ftplib.FTP(domain)
-    f.login("anonymous", "anonymous")
-    try:
-        logger.debug("Checking {}... Done.".format(url))
-        f.cwd(os.path.dirname(path))
-        file_present = False
-        if os.path.basename(parsed.path) in f.nlst():
-            file_present = True	
-        f.quit()
-        return file_present
-    except ftplib.all_errors as e:
-        logger.debug("{} not a valid path on {}".format(path, domain))
-        f.quit()
-        return False
+    for uri_field, domains in ftp_uris.items():
+        for domain, uris in domains.items():
+            f = ftplib.FTP(domain)
+            f.login("anonymous", "anonymous")
+            for uri in uris:
+                try:
+                    logger.debug("Checking {}... Done.".format(uri["uri"]))
+                    f.cwd(os.path.dirname(uri["path"]))
+
+                    if not os.path.basename(uri["path"]) in f.nlst():
+                        invalid_uris[uri_field].append(uri["uri"])
+
+                except ftplib.all_errors as e:
+                    logger.debug("{} not a valid path on {}".format(uri["path"], domain))
+                    invalid_uris[uri_field].append(uri["uri"])
+            f.quit()
 
 
 def is_valid_url(url, logger=None, retry=10):
     """Check if a given URL exists without downloading the page/file
 
     For HTTP and HTTPS URLs, urllib.requests returns a http.client.HTTPResponse object.
-    For FTP URLs we check with ftplib to avoid incomplete requests.
     """
 
-    parsed = urllib.parse.urlparse(url)
-    if parsed.scheme == "ftp":
-        return is_valid_ftp(url, logger=logger)
-    else:
-        # The global timeout for waiting for the response from the server before giving up
-        timeout = 2
-        socket.setdefaulttimeout(timeout)
+    # The global timeout for waiting for the response from the server before giving up
+    timeout = 5
+    socket.setdefaulttimeout(timeout)
 
-        try:
-            r = urllib.request.urlopen(url)
-            logger.debug("Checking {}... Done.".format(url))
-            if r:
-                return True
-        except urllib.error.URLError:
-            if retry > 0:
-                logger.debug("URI check failed for {}. Retrying {} more time(s).".format(url, str(retry)))
-                time.sleep(60/retry)
-                return is_valid_url(url, logger, retry-1)
-            return False
+    try:
+        r = urllib.request.urlopen(url)
+        logger.debug("Checking {}... Done.".format(url))
+        if r:
+            return True
+    except (urllib.error.URLError, socket.timeout) as e:
+        if retry > 0:
+            logger.debug("URI check failed for {}. Retrying {} more time(s).".format(url, str(retry)))
+            time.sleep(60/retry)
+            return is_valid_url(url, logger, retry-1)
+        return False
+
+
+def check_urls(logger, uris_to_check):
+    invalid_uris = defaultdict(list)
+    ftp_uris = defaultdict(lambda: defaultdict(list))
+    other_uris = defaultdict(list)
+
+    for uri_field, uris in uris_to_check.items():
+
+        # collecting FTP urls to make one connection and check them all
+        for uri in uris:
+            parsed = urllib.parse.urlparse(uri)
+            if parsed.scheme == "ftp":
+                # group them by domain "parsed.netloc"
+                ftp_uris[uri_field][parsed.netloc].append({"uri": uri, "path": parsed.path})
+            else:
+                other_uris[uri_field].append(uri)
+
+    if ftp_uris:
+        check_ftp_uris(ftp_uris, invalid_uris, logger)
+
+    if other_uris:
+        for uri_field in other_uris:
+            # Using multi-threading to speed up the validation of web URIs
+            with PoolExecutor(max_workers=20) as executor:
+                executor.submit(is_valid_url, other_uris, logger)
+                future_to_url = {executor.submit(is_valid_url, uri, logger): uri for uri in other_uris[uri_field]}
+                for future in concurrent.futures.as_completed(future_to_url):
+                    if future.result() is False:
+                        invalid_uris[uri_field].append(future_to_url[future])
+
+    return invalid_uris
 
 
 def get_controlled_vocabulary(category, resource="atlas", logger=None):
